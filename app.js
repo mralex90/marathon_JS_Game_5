973c45e3ad99eccfde28b9557c584d87a252b18a
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-26 14:51:33 +0300

    board/ finish Game

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
-let time = 11
+let time = 0
+let score = 0
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
@@ -18,8 +19,13 @@ timeList.addEventListener('click', event => {
     }
 })
 
-// DEBUG
-startGame()
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
 
 function startGame() {
     setInterval(decreaseTime, 1000)
@@ -44,7 +50,8 @@ function setTime(value) {
 }
 
 function finishGame(){
-
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
 }
 
 function createRandomCircle(){
