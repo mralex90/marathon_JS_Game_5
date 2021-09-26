59ff884575967cfa29e4449acd2620066e3b355c
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-26 15:24:17 +0300

    homework

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+
 let time = 0
 let score = 0
 
@@ -49,12 +50,12 @@ function setTime(value) {
     timeEl.innerHTML = `00:${value}`
 }
 
-function finishGame(){
+function finishGame() {
     timeEl.parentNode.classList.add('hide')
     board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
 }
 
-function createRandomCircle(){
+function createRandomCircle() {
     const circle = document.createElement('div')
     const size = getRandomNumber(10, 60)
     const {width, height} = board.getBoundingClientRect()
@@ -66,10 +67,11 @@ function createRandomCircle(){
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
-
+    
     board.append(circle)
 }
 
 function getRandomNumber(min, max){
     return Math.round(Math.random() * (max - min) + min)
 }
+
