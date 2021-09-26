600aa1397244063f72b3e306374b2a5857da4db7
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-26 14:36:36 +0300

    board/destructuring

--- app.js
+++ app.js
@@ -50,10 +50,15 @@ function finishGame(){
 function createRandomCircle(){
     const circle = document.createElement('div')
     const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
 
     circle.classList.add('circle')
     circle.style.width = `${size}px`
     circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
 
     board.append(circle)
 }
