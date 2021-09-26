80121595c4c305c0412177e3a3613cc2448f9e09
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-26 13:54:13 +0300

    delegation of events

---
+++ app.js
@@ -0,0 +1,19 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+let time = 0
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'))
+        startGame()
+    }
+})
+
+function startGame() {
+    screens[1].classList.add('up')
+}
