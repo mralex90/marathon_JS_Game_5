5fa61f8641bb821937e6a9f6caf126afb39569aa
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-26 13:59:32 +0300

    time-list

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const startBtn = document.querySelector('#start')
 const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
-let time = 0
+const timeEl = document.querySelector('#time')
+let time = 20
+
 startBtn.addEventListener('click', (event) => {
     event.preventDefault()
     screens[0].classList.add('up')
@@ -10,10 +12,14 @@ startBtn.addEventListener('click', (event) => {
 timeList.addEventListener('click', event => {
     if (event.target.classList.contains('time-btn')) {
         time = parseInt(event.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
         startGame()
     }
 })
 
+// DEBUG
+startGame()
+
 function startGame() {
-    screens[1].classList.add('up')
+    timeEl.innerHTML = `00:${time}`
 }
